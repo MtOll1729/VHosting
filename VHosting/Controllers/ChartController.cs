53f7f44692688d8f5d5c4db66d63fbb08a5ed7f9
DimonD <dprohorchuk2002@gmail.com>
2022-05-01 11:41:25 +0300

    Chart (just a bit)

---
+++ VHosting/Controllers/ChartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace VHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly DBVideoHostingContext _context;
+        public ChartController(DBVideoHostingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var users = _context.Users.Include(x => x.Subsribers).ToList();
+            List<object> usersInfo = new List<object>();
+
+            usersInfo.Add(new[] {"User", "Videos"});
+            foreach(var user in users)
+            {
+                usersInfo.Add(new object[] { user.Nickname, user.Subsribers.Count });
+            }
+
+            return new JsonResult(usersInfo);
+        }
+    }
+}
