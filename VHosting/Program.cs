83687ea32f95ba2e66e81c196fb3b93e605064fb
DimonD <dprohorchuk2002@gmail.com>
2022-05-01 18:32:45 +0300

    Roles

--- VHosting/Program.cs
+++ VHosting/Program.cs
@@ -16,6 +16,23 @@ builder.Services.AddMvc();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole<int>>>();
+        await RoleInitializer.InitializeAsync(userManager, rolesManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database." + DateTime.Now.ToString());
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -29,8 +46,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
---
+++ VHosting/RoleInitializer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VHosting
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            string nickname = "Knight";
+            string adminEmail = "da@da.com";
+            string password = "1234-asD";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("user"));
+            }
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = nickname, Nickname = nickname, Password = password, AccountType=1};
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+
+    }
+}
