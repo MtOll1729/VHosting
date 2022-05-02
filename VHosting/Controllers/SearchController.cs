c9ad03436eb49ff4902ab505e6190148f51e21ef
DimonD <dprohorchuk2002@gmail.com>
2022-05-02 10:47:04 +0300

    SearchEngine

---
+++ VHosting/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VHosting.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly DBVideoHostingContext _context;
+        public SearchController(DBVideoHostingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string searched)
+        {
+            var list = new List<object>();
+
+            foreach(var user in _context.Users)
+            {
+                if (user.Nickname.Contains(searched, System.StringComparison.CurrentCultureIgnoreCase)) list.Add(user);
+            }
+
+            foreach (var video in _context.Videos)
+            {
+                if (video.Name.Contains(searched, System.StringComparison.CurrentCultureIgnoreCase)) list.Add(video);
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ VHosting/Controllers/VideosController.cs
@@ -0,0 +1,160 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VHosting;
+
+namespace VHosting.Controllers
+{
+    public class VideosController : Controller
+    {
+        private readonly DBVideoHostingContext _context;
+
+        public VideosController(DBVideoHostingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Videos
+        public async Task<IActionResult> Index()
+        {
+            var dBVideoHostingContext = _context.Videos.Include(v => v.User);
+            return View(await dBVideoHostingContext.ToListAsync());
+        }
+
+        // GET: Videos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // GET: Videos/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Videos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,Name,Description,Link,Likes,Dislikes,IsMonetized")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(video);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", video.UserId);
+            return View(video);
+        }
+
+        // GET: Videos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", video.UserId);
+            return View(video);
+        }
+
+        // POST: Videos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Name,Description,Link,Likes,Dislikes,IsMonetized")] Video video)
+        {
+            if (id != video.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(video);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VideoExists(video.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", video.UserId);
+            return View(video);
+        }
+
+        // GET: Videos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // POST: Videos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            _context.Videos.Remove(video);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VideoExists(int id)
+        {
+            return _context.Videos.Any(e => e.Id == id);
+        }
+    }
+}
